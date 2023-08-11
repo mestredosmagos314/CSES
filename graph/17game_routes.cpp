2a001a233d1fcfd29c5bf6757ff9bcfbf98d7077
mestredosmagos314 <caiodavimonteiro.20231@poli.ufrj.br>
2023-08-11 15:47:28 -0300

    added the solution for problem 17

---
+++ graph/17game_routes.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define nl '\n'
+#define INF (int) 1e9
+#define BINF (ll) 1e18
+
+int n, m;
+vector <int> adj[100005];
+vector <int> deg(100005);
+queue <int> q;
+vector <int> dp(100005);
+vector <int> pre(100005);
+int main(){
+
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    //input
+    cin >> n >> m;
+    for(int i = 1; i <= n; ++i){
+        deg[i] = 0;
+    }
+    for(int i = 0; i < m; ++i){
+        int tmp1, tmp2;
+        cin >> tmp1 >> tmp2;
+        adj[tmp2].pb(tmp1);
+        deg[tmp1]++;
+    }
+    
+    
+    //processing
+    for(int i = 1; i <= n; ++i){
+        dp[i] = 0;
+        if(deg[i] == 0){
+            q.push(i);
+        }
+    }
+
+    dp[n] = 1;
+
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        for(int i = 0; i < adj[curr].size(); ++i){
+            int nei = adj[curr][i];
+            dp[nei] += dp[curr];
+            dp[nei] %= (int) 1e9 + 7;
+            deg[nei]--;
+            if(deg[nei] == 0){
+                q.push(nei);
+            }
+        }
+    }
+    //output
+    cout << dp[1];
+
+    return 0;
+}
